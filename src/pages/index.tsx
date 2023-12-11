7f61eab892e72a7c3147ddce47610a8fcef0ba07
currents-sagar <sagar@currents.ai>
2023-12-11 16:59:10 +0530

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,115 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState } from "react"
 
-const inter = Inter({ subsets: ['latin'] })
+export default function Index() {
+  // State declarations
+  const [array, setArray] = useState<number[]>([])
+  const [inputValue, setInputValue] = useState<string>("")
+  const [isSorted, setIsSorted] = useState<boolean>(false)
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false)
 
-export default function Home() {
+  const buttonClasses =
+    "bg-zinc-900 px-8 py-2 rounded-md border active:opacity-70 hover:opacity-90 border-zinc-700"
+
+  // Function to handle bubble sort step
+  const handleBubbleSort = () => {
+    const arr = [...array]
+    let sorted = true
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        ;[arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+        sorted = false
+        break // Exit loop after first swap
+      }
+    }
+    setArray(arr)
+    setIsSorted(sorted)
+
+    // Alert when sorting is complete
+    if (sorted) {
+      alert("Array is completely sorted!")
+    }
+  }
+
+  // Function to handle input submission
+  const handleSubmit = () => {
+    if (!inputValue) {
+      alert("Enter numbers separated by commas")
+      return
+    }
+    const numbers = inputValue.split(",").map((num) => parseInt(num.trim(), 10))
+    setArray(numbers)
+    setIsSubmitted(true)
+  }
+
+  // Function to reset the state
+  const handleReset = () => {
+    setArray([])
+    setInputValue("")
+    setIsSorted(false)
+    setIsSubmitted(false)
+  }
+
+  // Calculate the maximum value for dynamic height scaling
+  const maxValue = array.length > 0 ? Math.max(...array) : 0
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col w-screen h-screen justify-center items-center">
+      <div className="min-w-[40rem] space-y-16 p-10 bg-zinc-950 rounded-md border border-zinc-700 shadow-cyan-500/10 shadow-lg">
+        {!isSubmitted ? renderForm() : renderSortDeck()}
+      </div>
+    </div>
+  )
+
+  function renderForm() {
+    return (
+      <div className="space-y-4">
+        <label className="block mb-2 text-xs dark:text-zinc-200 font-medium">
+          Enter numbers separated by commas
+        </label>
+        <input
+          type="text"
+          className="border border-gray-300 text-white text-sm block w-full p-2.5 dark:bg-zinc-900 dark:border-zinc-950 rounded-md"
+          placeholder="1,4,5,6,7"
+          value={inputValue}
+          autoFocus
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <div className="flex justify-end items-center">
+          <button onClick={handleSubmit} className={buttonClasses}>
+            Start Sorting
+          </button>
         </div>
       </div>
+    )
+  }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  function renderSortDeck() {
+    return (
+      <>
+        <div className="flex justify-between items-end w-full space-x-4 h-64 ">
+          {array.map((num, index) => (
+            <div
+              key={index}
+              className="bg-cyan-400 w-full border border-black animate-loadFadeUp flex justify-center items-center text-xl font-bold text-zinc-700"
+              style={{ height: `${(num / maxValue) * 100}%` }}
+            >
+              {num}
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-end w-full space-x-4">
+          <button
+            className={buttonClasses}
+            onClick={handleBubbleSort}
+            disabled={isSorted}
+          >
+            Step Sort
+          </button>
+          <button onClick={handleReset} className={buttonClasses}>
+            Reset
+          </button>
+        </div>
+      </>
+    )
+  }
 }
